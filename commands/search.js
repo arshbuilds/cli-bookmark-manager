f1d300a1764c698075af8b80726a89c7db4cac53
arshbuilds <arshisha2516@gmail.com>
2025-10-09 18:40:24 +0530

    feat: fuzzy search

--- commands/search.js
+++ commands/search.js
@@ -1,12 +1,13 @@
 import { Command } from "commander";
 import fs from "fs";
 import path from "path";
+import Fuse from "fuse.js";
 
 const DATA_FILE = path.join(process.cwd(), "bookmarks.json");
 
 function loadBookMarks() {
   if (!fs.existsSync(DATA_FILE)) return [];
-  
+
   try {
     const data = fs.readFileSync(DATA_FILE, "utf8");
     return data ? JSON.parse(data) : [];
@@ -22,21 +23,27 @@ const searchCommand = new Command("search")
   .option("-a, --all", "shows all bookmarks")
   .action((query, options) => {
     let bookmarks = loadBookMarks();
-    let results;
     if (options.all) {
-      results = bookmarks;
-    } else if (query) {
-      results = bookmarks.filter(
-        (b) =>
-          b.title.toLowerCase().includes(query.toLowerCase()) ||
-          b.url.toLowerCase().includes(query.toLowerCase())
-      );
-    }else{
+        console.log("Showing all ${bookmarks.length} bookmarks:");
+        console.log(bookmarks);
+        return
+    } 
+
+    if (!query) {
         console.log("Please provide a query or use --all to show all bookmarks.");
         return;
+    }  
+    const fuse = new Fuse(bookmarks, {
+      keys: ["title", "url", "tags", "notes"],
+      threshold: 0.3,
+    });
+    const results = fuse.search(query).map(result => result.item);
+     if (results.length === 0) {
+      console.log(`❌ No bookmarks found for "${query}"`);
+    } else {
+      console.log(`🔎 Found ${results.length} result(s) for "${query}":`);
+      results.forEach(b => console.log(`- [${b.id}] ${b.title} (${b.url})`));
     }
-
-    console.log(results);
-  });
+});
 
 export default searchCommand;
